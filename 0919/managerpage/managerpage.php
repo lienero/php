91498939c464ebdc28c2f80e405a0c84ff11b2ed
이경민 <lienero@naver.com>
2020-11-02 10:01:24 +0900

    1102 매니저페이지 수정 임시

--- 0919/managerpage/managerpage.php
+++ 0919/managerpage/managerpage.php
@@ -1,7 +1,7 @@
 <?php
-  include "../db/db.php";
+  include $_SERVER['DOCUMENT_ROOT']."/recipe_site/mainpage/header.php";
   $sql_mem = mq("select mem_id, mem_email, mem_spicy from po_member");
-  $sql_recipe = mq("select recipe_name, mem_id, recipe_seq from po_recipe");
+  $sql_review = mq("select review_name, mem_id, review_seq from po_review");
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -24,8 +24,17 @@
       });
     });
   </script>
-  <link rel="stylesheet" href="../css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/mypage.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../js/bootstrap.js">
+    <link rel="stylesheet" href="../js/npm.js">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
 </head>
 
 <body>
@@ -33,7 +42,7 @@
   <div class="container-fluid" style="border: 1px solid black;">
     <!-- 이것은 콘텐츠의 row(가로 열)를 작성하기 위한 컨테이너이다. 그리드 시스템에서는 이 class="row" 태그 안에 표시할 내용을 준비한다. 
            그로 인해, 이 class="row" 태그 안에 포함된 콘텐츠의 태그가 자동으로 가로로 나란히 정렬되거나, 세로로 정렬되거나 한다.-->
-    <div class="row">
+    <div class="row" style ="width : 100%;">
       <!-- 폭 조정을 하는 클래스 col-종류-숫자 형태로 작성한다 md:태블릿-->
       <div class="col-md-12">
         <!-- class="page-header"라는 스타일은 헤더 부분의 스타일 클래스-->
@@ -43,36 +52,36 @@
           <img src="../signup/img//logo.png" alt="" width="200" class="img-responsive left-block" />
         </div>
         <!-- <form class="form-inline"> : 입력폼의 입력 항목이 가로로 표시되게 된다. -->
-        <form class="form-inline" action="./recipe_delete.php" method="POST">
+        <form class="form-inline" action="./review_delete.php" method="POST">
           <fieldset>
             <legend>작성된 후기리스트</legend>
             <table class="table table-bordered">
               <tr class="info">
                 <th>
                   <label>
-                    <input type="checkbox" value="all" class="check_all" name="recipe" />&nbsp;선택
+                    <input type="checkbox" value="all" class="check_all" name="review" />&nbsp;선택
                   </label>
                 </th>
                 <th>제목</th>
                 <th colspan="3">작성자</th>
               </tr>
-              <!-- start recipe list -->
-              <?
-                while($recipe = $sql_recipe->fetch_array()) {
+              <!-- start review list -->
+              <?php
+                while($review = $sql_review->fetch_array()) {
                   echo <<< html
                   <tr>
                     <td>
                       <label>
-                        <input type="checkbox" value="$recipe[2]" name="recipe[]" class="check" />
+                        <input type="checkbox" value="$review[2]" name="review[]" class="check" />
                       </label>
                     </td>
-                    <td>$recipe[0]</td>
-                    <td>$recipe[1]</td>
+                    <td>$review[0]</td>
+                    <td>$review[1]</td>
                   </tr>
 html;
                 }
               ?>
-              <!-- end recipe list -->
+              <!-- end review list -->
             </table>
             <div><input type="submit" class="btn btn-info pull-right" value="게시물 삭제"></div>
           </fieldset>
@@ -92,7 +101,7 @@ html;
                 <th style="width: 78%;">아이디</th>
               </tr>
               <!-- start member list -->
-              <?
+              <?php
                 while($member = $sql_mem->fetch_array()) {
                   echo <<< html
                   <tr>
