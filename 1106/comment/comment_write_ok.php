86923bdf882bd15d5c542cc732f331f111d3e1f8
이경민 <lienero@naver.com>
2020-12-21 13:23:19 +0900

    1221 코멘트 이미지 미 입력시 출력 오류 수정

--- 1106/comment/comment_write_ok.php
+++ 1106/comment/comment_write_ok.php
@@ -15,16 +15,20 @@ $content = $_POST['content'];
 //현재 시간을 변수에 대입한다.
 $time = DATE("Y-m-d H:i:s", time());
 $recipe_seq = $_GET['recipe_seq'];
-
 // $_FILES['userfile']['tmp_name'] : 서버에 저장된 업로드된 파일의 임시 파일 이름. 
 $tmpfile = $_FILES['image_link']['tmp_name'];
 //$_FILES['userfile']['name'] : 클라이언트 머신에 존재하는 파일의 원래 이름. 
 $o_name = $_FILES['image_link']['name'];
-// iconv(문자열 charset, 변경할 charset, 문자열) : 문자열을 요청 된 문자 인코딩으로 변환합니다.
-$filename = iconv("UTF-8", "EUC-KR//IGNORE", ($com_seq+1)."_com_img.jpeg");
-$folder = $_SERVER['DOCUMENT_ROOT']."/recipe_site/img/".$filename;
-//move_uploaded_file(서버로 전송된 파일, 지정위치)은 서버로 전송된 파일을 폴더에 저장할 때 사용하는 함수입니다.
-move_uploaded_file($tmpfile,$folder);
+
+if($tmpfile){
+    // iconv(문자열 charset, 변경할 charset, 문자열) : 문자열을 요청 된 문자 인코딩으로 변환합니다.
+    $filename = iconv("UTF-8", "EUC-KR//IGNORE", ($com_seq+1)."_com_img.jpeg");
+    $folder = $_SERVER['DOCUMENT_ROOT']."/recipe_site/img/".$filename;
+    //move_uploaded_file(서버로 전송된 파일, 지정위치)은 서버로 전송된 파일을 폴더에 저장할 때 사용하는 함수입니다.
+    move_uploaded_file($tmpfile,$folder);
+} else {
+    $filename = "";
+}
 
 // $etc = $_POST['etc'];
 
