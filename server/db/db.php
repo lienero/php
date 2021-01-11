955e8d6509d518e294fa16b46b597c7c8dcbbab1
이경민 <lienero@naver.com>
2021-01-11 17:21:56 +0900

    0111 서버에 맞춘 데이터베이스 명 설정

--- server/db/db.php
+++ server/db/db.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   
-  $db = new mysqli("localhost","w1004mesm","smagics1s2s3","p1");
+  $db = new mysqli("localhost","w1004mesm","smagics1s2s3","w1004mesm");
   $db->set_charset("utf8");
 
   function mq($sql){
